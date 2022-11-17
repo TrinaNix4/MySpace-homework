ec61e28564fd1f8ac7e1e3161231947f24552e2d
Trina Nixon <TrinaNixon7@gmail.com>
2022-11-17 16:23:55 -0700

    deleted about page

---
+++ client/src/components/shared/About.js
@@ -0,0 +1,32 @@
+// // import React from 'react';
+// // import { render } from 'react-dom';
+
+// // const About = () => {
+
+// // return(
+// // <div>
+// //   <div class="about-section">
+// //     <h1>We are Memestr</h1>
+// //       <p>Memestr makes it easy to discover and enjoy all of your favorite memes. </p>
+// //       <p>Simply register as a new user, or login to experience all that Memestr offers, including: adding a new meme, commenting on memes, like/dislike memes, and more!</p>
+// //   </div>
+
+// //     <h2 style="text-align:center">Our Team</h2>
+// //   <div class="row">
+// //     <div class="column">
+// //       <div class="card">
+// //         <img src="/w3images/team1.jpg" alt="Jane" style="width:100%"/>
+// //           <div class="container">
+// //             <h2>Trina</h2>
+// //               <p class="title">Developer</p>
+// //               <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+// //               <p>jane@example.com</p>
+// //               <p><button class="button">Contact</button></p>
+// //           </div>
+// //       </div>
+// //     </div>
+// //   </div>
+// //   )
+// // };
+
+// export default About;
